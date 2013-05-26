63f68ac337d5931bcefd6864c2bdbad81cf6f559
Esa Varemo <esa@kuivanto.fi>
2013-05-26 13:32:03 +0300

    cleaned up ignored death causes an possibly fixed ISSUE #006

--- src/main/java/fi/dy/esav/InfiniteFight/InfiniteFightEntityListener.java
+++ src/main/java/fi/dy/esav/InfiniteFight/InfiniteFightEntityListener.java
@@ -1,6 +1,9 @@
 package fi.dy.esav.InfiniteFight;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,11 +14,16 @@ import org.bukkit.event.entity.EntityDeathEvent;
 
 public class InfiniteFightEntityListener implements Listener {
 	
-	int num = 3;
+	static final List<DamageCause> ignoredCauses = Arrays.asList(DamageCause.DROWNING, DamageCause.FIRE, DamageCause.FIRE_TICK, DamageCause.LAVA, DamageCause.SUFFOCATION, DamageCause.VOID);
+	
+	int num = 1; // Debug, prevent explosions
 	
 	@EventHandler(priority= EventPriority.NORMAL)
 	public void onEntityDeath(EntityDeathEvent e) {
-		if (e.getEntity() instanceof LivingEntity && !(e.getEntity() instanceof Player && !(e.getEntity().getLastDamageCause().getCause() == DamageCause.FIRE || e.getEntity().getLastDamageCause().getCause() == DamageCause.FIRE_TICK || e.getEntity().getLastDamageCause().getCause() == DamageCause.LAVA || e.getEntity().getLastDamageCause().getCause() == DamageCause.VOID || e.getEntity().getLastDamageCause().getCause() == DamageCause.SUFFOCATION))) {
+		if (	e.getEntity() instanceof LivingEntity &&
+				!(e.getEntity() instanceof Player) && 
+				!(ignoredCauses.contains(e.getEntity().getLastDamageCause().getCause())) 
+			) {
 
 			for (int x=1;x<=num;x++) {
 				e.getEntity().getWorld().spawn(e.getEntity().getLocation(), e.getEntity().getClass());			
